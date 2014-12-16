be329187e71a41264b1f55763c027bc2d4f87330
Patryk Szeliga <patryk.szeliga@rst-it.com>
2014-12-16 10:31:55 +0100

    Small padding adjustment

--- app/controllers/backend/users_controller.rb
+++ app/controllers/backend/users_controller.rb
@@ -14,6 +14,7 @@ class Backend::UsersController < BackendController
       @user.account_id = current_user.account_id
       UserDecorator.decorate @user
       @user.save
+      redirect_to backend_users_path
     else
       render 'create_failure'
     end
